4816486003ef45448333b63468929a8a5610606c
Mohamed Hassan <mohahassanna933@gmail.com>
2024-06-19 10:27:25 +0300

    Onboarding Screen

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../core/routing/routes.dart';
+import '../../features/login/ui/login_screen.dart';
+import '../../features/onboarding/ui/onboarding_screen.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    final arguments = settings.arguments;
+
+    switch (settings.name) {
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text("No Routes defined for ${settings.name}"),
+            ),
+          ),
+        );
+    }
+  }
+}
---
+++ lib/features/onboarding/ui/widgets/get_started_button.dart
@@ -0,0 +1,32 @@
+import 'package:complet_project/core/helper/extentions.dart';
+import 'package:flutter/material.dart';
+import '../../../../core/routing/routes.dart';
+import '../../../../core/theme/colors.dart';
+import '../../../../core/theme/style.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        context.pushNamed(Routes.loginScreen);
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(ColorsManager.mainBlue),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: MaterialStateProperty.all(
+          const Size(double.infinity, 52),
+        ),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        ),
+      ),
+      child: Text(
+        'Get Started',
+        style: TextStyles.font16WhiteMedium,
+      ),
+    );
+  }
+}
